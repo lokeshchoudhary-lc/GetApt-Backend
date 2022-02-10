052bfb8892cca79e4bf391ff128b90f3f2c6ed64
Lokesh Choudhary <lokeshchoudhary8949@gmail.com>
2022-02-10 13:48:40 +0530

    feat: Add MongoDb Config

---
+++ utils/init_mongodb.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const mongouri = process.env.MONGODB_URI;
+
+const dbname = process.env.DB_NAME;
+
+mongoose.connect(mongouri, {
+  dbName: dbname,
+  // autoIndex: false, uncomment it in production
+});
+
+mongoose.connection.on('connected', () => console.log('Connected to database'));
+mongoose.connection.on('error', (err) => console.log(err.message));
+mongoose.connection.on('disconnected', () =>
+  console.log('Disconnected to database')
+);
+
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
